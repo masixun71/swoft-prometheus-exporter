fa5158c13b1277b21724a2eea60ee2925e3a1feb
马斯珣 <masixun@baijiahulian.com>
2018-08-09 17:10:45 +0800

    bugfix

--- src/Middlewares/InitPrometheusExporterMiddleware.php
+++ src/Middlewares/InitPrometheusExporterMiddleware.php
@@ -54,25 +54,24 @@ class InitPrometheusExporterMiddleware implements MiddlewareInterface
         $uri = $request->getSwooleRequest()->server['request_uri'];
         $uri = ltrim($uri, '/');
         $uri = str_replace('/', '_', $uri);
+        $startMicroTime = (microtime(true) * 1000);
+        $response = $handler->handle($request);
+        $endMicroTime = (microtime(true) * 1000);
+        $value = ($endMicroTime - $startMicroTime) / 1000;
         if (!empty($uri))
         {
             $this->collectorRegistry->counterIncr('http_request', 'total', 1, ['source' => 'all']);
-            $startMicroTime = (microtime(true) * 1000);
-            $response = $handler->handle($request);
-            $endMicroTime = (microtime(true) * 1000);
-            $value = ($endMicroTime - $startMicroTime) / 1000;
             $this->collectorRegistry->counterIncr('http_request', 'total', 1, ['source' => 'api', 'uri' => $uri, 'code' => (string)$response->getStatusCode()]);
             $this->collectorRegistry->histogramIncr('http_request', 'duration_seconds', $value, ['source' => 'all']);
             $this->collectorRegistry->histogramIncr('http_request', 'duration_seconds', $value, ['source' => 'api', 'uri' => $uri]);
 
-        }
-
-        if (isset(PECacheTableCollector::getCollector()[RequestContext::getContextData()['controllerClass']][RequestContext::getContextData()['controllerAction']]))
-        {
-            $cacheTable = PECacheTableCollector::getCollector()[RequestContext::getContextData()['controllerClass']][RequestContext::getContextData()['controllerAction']];
-            if ($cacheTable)
+            if (isset(PECacheTableCollector::getCollector()[RequestContext::getContextData()['controllerClass']][RequestContext::getContextData()['controllerAction']]))
             {
-                $this->collectorRegistry->cacheTable();
+                $cacheTable = PECacheTableCollector::getCollector()[RequestContext::getContextData()['controllerClass']][RequestContext::getContextData()['controllerAction']];
+                if ($cacheTable)
+                {
+                    $this->collectorRegistry->cacheTable();
+                }
             }
         }
 
